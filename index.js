58246bf87178e3a6e24bec5735ffbe58e71799d3
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-06-20 05:28:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function iterativeLog(array) {
     console.log(`${index}: ${element}`);
   });
 }
+
+function iterate(callback) {
+  var iterable = [1, 2, 3, 4];
+  iterable.forEach(callback);
+
+  return iterable;
+}
